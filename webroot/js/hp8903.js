a9a107d89c0c1ca6a2aaa92fb86efd686ab77647
Tobias Heide <theide@stereokrauts.com>
2020-09-27 16:38:24 +0200

    option to keep old measurements

--- webroot/js/hp8903.js
+++ webroot/js/hp8903.js
@@ -1,5 +1,8 @@
 
 function measure_success(data, textStatus, jqXHR) {
+    if ($("#keep-measurements").val()) {
+        $("#output_image").clone().prepend("#output_old");
+    }
     $("#output_image").attr("src", "data:image/png;base64," + data);
 }
 
@@ -28,23 +31,27 @@ function measure(url, type, steps, freq1, freq2, amp1, amp2, title) {
     })
 }
 
+function submit_measure(event) {
+    // Stop form from submitting normally
+    event.preventDefault();
+
+    // Get some values from elements on the page:
+    var $form = $( this ),
+    type = $form.find( "select[name='type']" ).val(),
+    steps = $form.find( "input[name='steps']" ).val(),
+    freq1 = $form.find( "input[name='freq1']" ).val(),
+    freq2 = $form.find( "input[name='freq2']" ).val(),
+    amp1 = $form.find( "input[name='amp1']" ).val(),
+    amp2 = $form.find( "input[name='amp2']" ).val(),
+    title = $form.find( "input[name='title']" ).val()
+
+    url = $form.attr( "action" );
+
+    measure(url, type, steps, freq1, freq2, amp1, amp2, title);
+}
+
 function init_hp8903() {
     $( "#measure-form" ).submit(function( event ) {
-        // Stop form from submitting normally
-        event.preventDefault();
-
-        // Get some values from elements on the page:
-        var $form = $( this ),
-        type = $form.find( "select[name='type']" ).val(),
-        steps = $form.find( "input[name='steps']" ).val(),
-        freq1 = $form.find( "input[name='freq1']" ).val(),
-        freq2 = $form.find( "input[name='freq2']" ).val(),
-        amp1 = $form.find( "input[name='amp1']" ).val(),
-        amp2 = $form.find( "input[name='amp2']" ).val(),
-        title = $form.find( "input[name='title']" ).val()
-
-        url = $form.attr( "action" );
-
-        measure(url, type, steps, freq1, freq2, amp1, amp2, title);
+        submit_measure(event);
     });
 }
